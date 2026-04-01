eea26858e1353656ae266b94a24699ed776fc98a
nodo343 <nodikogamer01@gmail.com>
2026-04-01 14:31:41 +0400

    Update serve.py

--- serve.py
+++ serve.py
@@ -33,6 +33,7 @@ class AppHandler(SimpleHTTPRequestHandler):
         ".html": "text/html; charset=utf-8",
         ".js": "application/javascript; charset=utf-8",
         ".json": "application/json; charset=utf-8",
+        ".webmanifest": "application/manifest+json; charset=utf-8",
     }
 
     def __init__(self, *args, **kwargs):
